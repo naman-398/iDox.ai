af6f0d82744458239e8ee589b3cdde4b4ea526d0
Naman Jyani <namanjyani@Namans-MacBook-Air-2.local>
2023-11-24 20:19:35 +0530

    page-complete

---
+++ assets/js/add.js
@@ -0,0 +1,33 @@
+// preloder
+setTimeout(() => {
+  document.getElementById("preloder").classList.add("d-none");
+  document.body.classList.remove("overflow-hidden");
+}, 2500);
+//back to top
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("back-to-top");
+  
+    // Show the button when the user scrolls down 200px from the top
+    window.onscroll = function () {
+      if (
+        document.body.scrollTop > 200 ||
+        document.documentElement.scrollTop > 200
+      ) {
+        button.style.display = "block";
+      } else {
+        button.style.display = "none";
+      }
+    };
+  
+    // Scroll back to the top when the button is clicked
+    button.onclick = function () {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    };
+  });
+//nav-bar
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu").classList.toggle("cross")
+}
